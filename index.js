c1a0b42127b211ff7df8c8975ce8ab39767837c9
nikhilpatill18 <nikhilpatil6060@gmail.com>
2025-02-15 11:36:51 +0530

    added signup and login page

--- index.js
+++ index.js
@@ -18,27 +18,40 @@ x_icon.addEventListener('click', () => {
 })
 // console.log(window.scrollY)
 
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 500) {
-        document.querySelector("nav").style.backgroundColor = 'rgba(9, 9, 9, 0.8)'
-        // document.querySelector("nav").style.height = '80px'
-    }
-    else {
-        document.querySelector("nav").style.background = 'black '
-        // document.querySelector("nav").style.height = '100px'
-    }
-})
 
-
-const navoption = document.querySelectorAll(".solution,.products,.get-app")
-navoption.forEach(option => {
-    option.addEventListener("mouseover", () => {
-
-        document.querySelector("nav").style.backgroundColor = 'black'
-
+if (window.location.pathname === "/index.html") {
+    console.log("User is on the Dashboard page.");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 500) {
+            document.querySelector("nav").style.backgroundColor = 'rgba(9, 9, 9, 0.8)'
+            // document.querySelector("nav").style.height = '80px'
+        }
+        else {
+            document.querySelector("nav").style.background = 'black '
+            // document.querySelector("nav").style.height = '100px'
+        }
     })
-    option.addEventListener("mouseout", () => {
-        document.querySelector("nav").style.backgroundColor = 'rgba(9, 9, 9, 0.8)'
 
+    const navoption = document.querySelectorAll(".solution,.products,.get-app")
+    navoption.forEach(option => {
+        option.addEventListener("mouseover", () => {
+
+            document.querySelector("nav").style.backgroundColor = 'black'
+
+        })
+        option.addEventListener("mouseout", () => {
+            document.querySelector("nav").style.backgroundColor = 'rgba(9, 9, 9, 0.8)'
+
+        })
     })
-})
+
+
+    function togetstarted() {
+        window.location.replace("http://127.0.0.1:5503/getstarted.html")
+    }
+
+}
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html',],
+  content: ['./index.html', './signup.html', './getstarted.html'],
   theme: {
     extend: {},
   },
